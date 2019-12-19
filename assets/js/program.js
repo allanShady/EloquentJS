93a27ccd50cd965e084bfa82470367559b47e099
Allan Camilo <acamilo.ag@gmail.com>
2019-12-19 10:29:29 +0200

    Class notation & overrides its obj properties

--- assets/js/program.js
+++ assets/js/program.js
@@ -1,9 +1,37 @@
 const samples = () => {
-    saySomething();
+    let whiteRabbit = {type: 'white', speak};
+    let hungryRabbit = {type: 'hungry rabbit', speak};
+
+    whiteRabbit.speak('I could use carrot right now');
+    //Explicit call of this
+    speak.call(hungryRabbit, 'Burp!');
+
+    //The rabbit class test
+    const killerRabbit = new Rabbit('killer');
+    const blackRabbit = new Rabbit('black');
+
+    console.log(`Testing the rabbit class`);
+    killerRabbit.speak('Nothing');
+    blackRabbit.speak('Murder');
+
+    console.log(`Overriding the proto`);
+    Rabbit.prototype.teeth = 'small';
+
+    console.log(killerRabbit.teeth.toString())
 }
 
-function saySomething(){
-    console.log('Testing sample btn')
+function speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`)
+}
+
+class Rabbit {
+    constructor(type) {
+        this.type = type;
+    }
+
+    speak(msg) {
+        console.log(`The ${this.type} rabbit says '${msg}'`);
+    }
 }
 
 sampleBtn.addEventListener('click', samples);
