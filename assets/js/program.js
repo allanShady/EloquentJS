27eeff66828a33c9d4d72567bc5f3d55f031fdc0
Allan Camilo <acamilo.ag@gmail.com>
2019-12-19 10:42:38 +0200

    Diff btwn plain obj & Map obj for Mapping values

--- assets/js/program.js
+++ assets/js/program.js
@@ -17,8 +17,23 @@ const samples = () => {
     console.log(`Overriding the proto`);
     Rabbit.prototype.teeth = 'small';
 
-    console.log(killerRabbit.teeth.toString())
+    console.log(killerRabbit.teeth.toString());
+
+    /*  Using object as map is dangerous
+        If you pass null to Object.create, the resulting object will not
+        derive from Object.prototype and can safely be used as a map. */
+    console.log(`Class Map`);
+    
+    let ages = new Map();
+    ages.set('Allan', 26);
+    ages.set('Kevin', 24);
+    ages.set('Cabral', 26);
+
+    console.log(`Allan is ${ages.get('Allan')}`);
+    console.log(`Salman's age known?`, ages.has('Salman'));
+    console.log(ages.has('toString'));
 }
+
 
 function speak(line) {
     console.log(`The ${this.type} rabbit says '${line}'`)
