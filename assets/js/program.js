992a7e25a5fda8cb7b263d014883aa0704d555df
Allan Camilo <acamilo.ag@gmail.com>
2019-12-25 17:36:34 +0200

    Iterator, getter & setter pratices

--- assets/js/program.js
+++ assets/js/program.js
@@ -32,8 +32,51 @@ const samples = () => {
     console.log(`Allan is ${ages.get('Allan')}`);
     console.log(`Salman's age known?`, ages.has('Salman'));
     console.log(ages.has('toString'));
+
+    //Simbols 
+    let sym = Symbol('kk');
+    console.log(sym === Symbol('kk'));
+    
+    //Interator interface using symbols
+    let okIterator = 'Ok'[Symbol.iterator]();
+    console.log(okIterator.next());
+    console.log(okIterator.next());
+    console.log(okIterator.next());
+
+    //geters & setters
+    const veryfingSize = {
+        get size() {
+            return Math.round(Math.random()*100);
+        }
+    }
+
+    console.log('GETTERS & SETTERS');
+    console.log(veryfingSize.size);
+
+    const tem = new Temperature(22);
+    console.log(tem.fahrenheit);
+
+    tem.fahrenheit = 86;
+    console.log(tem.celsius);
 }
 
+class Temperature {
+    constructor(celsius) {
+        this.celsius = celsius;
+    }
+
+    get fahrenheit() {
+        return this.celsius * 1.8 + 32;
+    }
+
+    set fahrenheit(value) {
+        this.celsius = (value - 32) / 1.8; 
+    } 
+
+    static fromFahrenheit(value) {
+        return new Temperature((value - 32) / 1.8);
+    }
+}
 
 function speak(line) {
     console.log(`The ${this.type} rabbit says '${line}'`)
