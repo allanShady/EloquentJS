902b744637751bb59b28c87743fbc0d44c494b35
Allan Camilo <acamilo.ag@gmail.com>
2019-12-25 19:45:10 +0200

    INHERITANCE

--- assets/js/program.js
+++ assets/js/program.js
@@ -58,6 +58,54 @@ const samples = () => {
 
     tem.fahrenheit = 86;
     console.log(tem.celsius);
+
+    console.log('INHERITANCE');
+    let matrix = new SymmetricMatrix(5, (x, y) => `${x},${y}`);
+    console.log(matrix.get(2, 3));
+    console.log(new SymmetricMatrix(2) instanceof SymmetricMatrix);
+}
+
+//classes 
+class Matrix {
+    constructor(width, height, element = (x, y) => undefined) {
+        this.width = width;
+        this.height = height;
+        this.content = [];
+        
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                this.content[y * width + x] = element(x, y);
+            }
+        }
+
+        console.log(this.content);
+    }
+    get(x, y) {
+        return this.content[y * this.width + x];
+    }
+    set(x, y, value) {
+        this.content[y * this.width + x] = value;
+    }
+}
+
+class SymmetricMatrix extends Matrix {
+    constructor(size, element = (x, y) => undefined) {
+        super(size, size, (x, y) => {
+        
+            if (x < y) 
+                return element(y, x);
+            else 
+                return element(x, y);
+        });
+    }
+
+    set(x, y, value) {
+        super.set(x, y, value);
+    
+        if (x != y) {
+            super.set(y, x, value);
+        }
+    }
 }
 
 class Temperature {
