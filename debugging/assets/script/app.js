7d7a2f9abc213b92d62fb818ec152b9decf2629a
Allan Camilo <acamilo.ag@gmail.com>
2020-01-09 11:18:54 +0200

    Cleaning up after exceptions

--- debugging/assets/script/app.js
+++ debugging/assets/script/app.js
@@ -58,7 +58,6 @@ function promptNumber(question) {
   else return result;
 }
 
-
 //Exceptions
 function promptDirection(question) {
   let result = prompt(question);
@@ -79,4 +78,35 @@ try {
   console.log("You see", look());
 } catch (error) {
   console.log("Something went wrong: " + error);
+}
+
+//Cleaning up after exceptions
+const accounts = {
+  a: 100,
+  b: 0,
+  c: 20
+};
+
+function getAccount() {
+  let accountName = prompt("Enter an account name");
+  if (!accounts.hasOwnProperty(accountName)) {
+    throw new Error(`No such account: ${accountName}`);
+  }
+  return accountName;
+}
+
+function transfer(from, amount) {
+  if (accounts[from] < amount) return;
+  let progress = 0;
+  
+  try {
+    accounts[from] -= amount;
+    progress = 1;
+    accounts[getAccount()] += amount;
+    progress = 2;
+  } finally {
+    if (progress == 1) {
+      accounts[from] += amount;
+    }
+  }
 }
