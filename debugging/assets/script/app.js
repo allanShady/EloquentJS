721130535375674390107449ea14145532a7c4ba
Allan Camilo <acamilo.ag@gmail.com>
2020-01-09 10:12:55 +0200

    Small program to  show how demo tests

--- debugging/assets/script/app.js
+++ debugging/assets/script/app.js
@@ -8,10 +8,28 @@ function canYouSpotTheProblem() {
 canYouSpotTheProblem();
 // → ReferenceError: counter is not defined
 
-"use strict";
+("use strict");
 function Person(name) {
   this.name = name;
 }
 let ferdinand = Person("Ferdinand"); // oops
 console.log(name);
 // → Ferdinan
+
+//Testing
+function test(label, body) {
+  if (!body()) console.log(`Failed: ${label}`);
+  else console.log("Passed.");
+}
+
+test("convert Latin text to uppercase", () => {
+  return "hello".toUpperCase() == "HELLO";
+});
+
+test("convert Greek text to uppercase", () => {
+  return "Χαίρετε".toUpperCase() == "ΧΑΊΡΕΤΕ";
+});
+
+test("don't convert case-less characters", () => {
+  return "ࢎ࠶߈ఆ௷".toUpperCase() == "ࢎ࠶߈ఆ௷";
+});
