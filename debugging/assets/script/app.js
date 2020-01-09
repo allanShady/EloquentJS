66907199bc4ffa8c1ade1a0103cf4a5303940455
Allan Camilo <acamilo.ag@gmail.com>
2020-01-09 11:06:29 +0200

    Demo on how to raise & handle the Exceptions

--- debugging/assets/script/app.js
+++ debugging/assets/script/app.js
@@ -50,3 +50,33 @@ function numberToString(n, base = 10) {
 }
 console.log(numberToString(13, 10));
 // → 1.5e-3231.3e-3221.3e-3211.3e-3201.3e-3191.3e…-3181.3
+
+//Error propagation
+function promptNumber(question) {
+  let result = Number(prompt(question));
+  if (Number.isNaN(result)) return null;
+  else return result;
+}
+
+
+//Exceptions
+function promptDirection(question) {
+  let result = prompt(question);
+  if (result.toLowerCase() == "left") return "L";
+  if (result.toLowerCase() == "right") return "R";
+  throw new Error("Invalid direction: " + result);
+}
+
+function look() {
+  if (promptDirection("Which way?") == "L") {
+    return "a house";
+  } else {
+    return "two angry bears";
+  }
+}
+
+try {
+  console.log("You see", look());
+} catch (error) {
+  console.log("Something went wrong: " + error);
+}
