7c230730a3d61fa9bc2a87e43eb6961f1bf92075
Allan Camilo <acamilo.ag@gmail.com>
2020-01-09 10:43:47 +0200

    Debugging

--- debugging/assets/script/app.js
+++ debugging/assets/script/app.js
@@ -33,3 +33,20 @@ test("convert Greek text to uppercase", () => {
 test("don't convert case-less characters", () => {
   return "ࢎ࠶߈ఆ௷".toUpperCase() == "ࢎ࠶߈ఆ௷";
 });
+
+//Debugging
+function numberToString(n, base = 10) {
+  let result = "",
+    sign = "";
+  if (n < 0) {
+    sign = "-";
+    n = -n;
+  }
+  do {
+    result = String(n % base) + result;
+    n = Math.floor(n/base);
+  } while (n > 0);
+  return sign + result;
+}
+console.log(numberToString(13, 10));
+// → 1.5e-3231.3e-3221.3e-3211.3e-3201.3e-3191.3e…-3181.3
